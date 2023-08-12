4485eb8bcdd279cdc01f5a24a9c29f01afe47b25
Arjit <arjitverma.go@gmail.com>
2023-08-12 14:42:21 +0530

    Router and Pages Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CategoryPage from "./pages/CategoryPage";
+import CartPage from "./pages/CartPage";
+import LoginPage from "./pages/LoginPage";
+import ProductPage from "./pages/ProductPage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="category" element={<CategoryPage />} />
+      <Route path="product" element={<ProductPage />} />
+      <Route path="cart" element={<CartPage />} />
+      <Route path="login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
